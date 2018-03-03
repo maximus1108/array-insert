1aa0d664487d82e1f4d2dd165f8eda1997be982a
maximus1108 <max.perry1108@yahoo.co.uk>
2018-03-03 21:45:45 +0000

    

--- config/karma.config.js
+++ config/karma.config.js
@@ -30,6 +30,9 @@ module.exports = function(config) {
       'test/*.js': ['babel', 'webpack']
     },
     
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
