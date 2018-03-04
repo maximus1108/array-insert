3cadebe0b54d7b3f19a5abbe0c06c0ecf08e2d2c
maximus1108 <max.perry1108@yahoo.co.uk>
2018-03-04 18:31:09 +0000

    fixed funcionality to insert by acending order using binary lookup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
-Array.prototype.insert = (item, evaluator) => {
-    console.log(item, evaluator);
+Array.prototype.insert = function (item, evaluator) {
+
+    if(item === undefined || item === null)
+        throw new Error (`You cannot insert a value of '${item}'`)
+
+    const arr = this;
+
+    const insertItem = (index) => {
+        arr.splice(index, 0, item);
+        return arr;
+    }
+
+    let minIndex = 0,
+        maxIndex = arr.length - 1,
+        currentIndex,        
+        currentValue,
+        adjacentIndex,
+        adjacentValue;
+
+    if(item >= arr[maxIndex]) {
+        arr.push(item);
+        return arr;
+    }
+
+    if(item <= arr[0]){
+        arr.unshift(item);
+        return arr;
+    }
+
+    while(minIndex <= maxIndex) {
+        currentIndex = Math.floor((minIndex + maxIndex) / 2);
+
+        currentValue = arr[currentIndex];
+
+        if(item === currentValue)
+            return insertItem(currentIndex);
+
+        else if (item < currentValue) {
+
+            adjacentIndex = currentIndex - 1
+            adjacentValue = arr[adjacentIndex];
+
+            if(item >= adjacentValue)
+                return insertItem(adjacentIndex);
+            else
+              maxIndex = adjacentIndex;
+
+        }
+        else if (item > currentValue) {
+            
+            adjacentIndex = currentIndex + 1;
+            adjacentValue = arr[adjacentIndex];
+
+            if(item <= adjacentValue)
+                return insertItem(adjacentIndex);
+            else           
+                minIndex = adjacentIndex;
+
+        }
+    }
+
+    return -1;
 }
--- test/test.js
+++ test/test.js
@@ -1,5 +1,9 @@
 import '../src/index.js';
 
+var b = [1,2,3,4,5,6].insert(1)
+
+console.log(b)
+
 describe("A suite is just a function", function() {
     var a;
   
